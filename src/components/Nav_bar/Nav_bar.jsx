d5ac7d6e458c7091bbbe1655cb820158e497d0f4
BetterLogicTeam <betterlogicslhr@gmail.com>
2022-08-23 22:30:39 -0700

    made responsive changes

--- src/components/Nav_bar/Nav_bar.jsx
+++ src/components/Nav_bar/Nav_bar.jsx
@@ -190,35 +190,7 @@ function Nav_bar() {
                     <Link to="/signup">Sign up</Link>
                       <i class="fa fa-caret-down"></i>
                     </a>
-                    {/* <div class="drop_c">
-                      <div class="row">
-                        <div class="column">
-                          <h3>video appoinments</h3>
-                          <p>
-                            The equivalent of our in-person appointments, but
-                            from anywhere! Schedule a virtual visit and let us
-                            help you get where you want to be physically!
-                          </p>
-                        </div>
-                        <div class="column">
-                          <h3>Training</h3>
-                          <p>
-                            Check out all of our online programs. They are
-                            created with our medical and training knowledge to
-                            help repair and prevent injury and enhance your
-                            mobility and strength!
-                          </p>
-                        </div>
-                        <div class="column">
-                          <h3>Custom programing</h3>
-                          <p>
-                            Have a specific goal and want to work 1-on-1 with
-                            one of our team members? We build a 100% custom
-                            program to help you with your needs and goals!
-                          </p>
-                        </div>
-                      </div>
-                    </div> */}
+                  
                   </div>
                 </div>
               </li>
@@ -229,35 +201,7 @@ function Nav_bar() {
                     <Link to="/login">Log in</Link>
                       <i class="fa fa-caret-down"></i>
                     </a>
-                    {/* <div class="drop_c">
-                      <div class="row">
-                        <div class="column">
-                          <h3>video appoinments</h3>
-                          <p>
-                            The equivalent of our in-person appointments, but
-                            from anywhere! Schedule a virtual visit and let us
-                            help you get where you want to be physically!
-                          </p>
-                        </div>
-                        <div class="column">
-                          <h3>Training</h3>
-                          <p>
-                            Check out all of our online programs. They are
-                            created with our medical and training knowledge to
-                            help repair and prevent injury and enhance your
-                            mobility and strength!
-                          </p>
-                        </div>
-                        <div class="column">
-                          <h3>Custom programing</h3>
-                          <p>
-                            Have a specific goal and want to work 1-on-1 with
-                            one of our team members? We build a 100% custom
-                            program to help you with your needs and goals!
-                          </p>
-                        </div>
-                      </div>
-                    </div> */}
+                  
                   </div>
                 </div>
               </li>
